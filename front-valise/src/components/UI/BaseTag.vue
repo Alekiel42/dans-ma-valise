4408e4f9603ace0e5e6023f4614eb0236cc25a9c
Alekiel42 <alexandra.bercoff@gmail.com>
2022-01-26 07:37:40 +0100

    front / basetag / improve css

--- front-valise/src/components/UI/BaseTag.vue
+++ front-valise/src/components/UI/BaseTag.vue
@@ -27,13 +27,19 @@ div {
   font-size: 0.8rem;
   color: white;
   background-color: $main-color;
+  border: 1px solid transparent;
   border-radius: 2rem;
   padding: 0.5rem;
   margin: 0.3rem;
+  transition: border 400ms ease-in-out;
+}
+
+div:hover {
+  border: 1px solid $main-color;
 }
 
 .unselected {
   color:  $dark-color;
-  background-color: white;
+  background-color: transparent;
 }
 </style>
